09004e73dff3063d670c01a21ecb786f1f373305
Ruthuran Muralirajan <ruthuranruthuran2@gmail.com>
2025-06-06 15:09:57 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,35 +1,38 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { Navbar, Nav, Container } from 'react-bootstrap';
-import { FaShoppingCart } from 'react-icons/fa'; 
-
+import { Link, NavLink } from 'react-router-dom';
+import { Navbar, Nav, Container, Badge } from 'react-bootstrap';
+import { FaShoppingCart } from 'react-icons/fa';
 
 function Header() {
   return (
     <Navbar bg="dark" variant="dark" expand="lg" className="navbar-wow">
       <Container>
-        
-        <Navbar.Brand href="/" className="navbar-brand">
+
+        <Navbar.Brand as={Link} to="/" className="navbar-brand d-flex align-items-center">
           <img
-            src="https://img.freepik.com/premium-vector/gadget-mobile-store-logo-branding-icon_1152716-1405.jpg" 
-            alt="Logo"
+            src="https://img.freepik.com/premium-vector/gadget-mobile-store-logo-branding-icon_1152716-1405.jpg"
+            alt="MyStore Logo"
             className="navbar-logo"
           />
-          <span className="navbar-brand-text">MyStore</span>
+          <span className="navbar-brand-text ms-2">MyStore</span>
         </Navbar.Brand>
 
         <Navbar.Toggle aria-controls="navbar-nav" />
         <Navbar.Collapse id="navbar-nav">
 
-          <Nav className="ml-auto navbar-links">
-            <Link className="nav-link" to="/products">
+          <Nav className="ms-auto navbar-links">
+            <NavLink className="nav-link" to="/products">
               <span className="nav-link-text">Products</span>
-            </Link>
-            <Link className="nav-link" to="/addcart">
-              <FaShoppingCart className="cart-icon" />
+            </NavLink>
+
+            <NavLink className="nav-link d-flex align-items-center" to="/addcart">
+              <FaShoppingCart className="cart-icon me-1" />
               <span className="nav-link-text">Cart</span>
-            </Link>
+              {/* Optional cart count badge */}
+              {/* <Badge bg="light" text="dark" className="ms-1">3</Badge> */}
+            </NavLink>
           </Nav>
+
         </Navbar.Collapse>
       </Container>
     </Navbar>
